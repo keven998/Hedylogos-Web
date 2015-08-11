e296d99a3291d4c76d5ea9cfe870ef320a51485a
lyn <buaaxigua@gmail.com>
2015-08-11 19:57:47 +0800

    add the logout and the alert msg

--- client/login/login.js
+++ client/login/login.js
@@ -2,15 +2,23 @@ Template.login.events({
   'click #login': function (e) {
     e.preventDefault();
     e.stopPropagation();
+
     var username = $('input[name="username"]').val(),
         password = $('input[name="password"]').val();
+
+    // 貌似有点多余...
     username = $.trim(username);
     password = $.trim(password);
+
     if (!username || !password) {
+      alert('账户和密码不能为空！');
       throwError('账户和密码不能为空！');
+      return ;
     }
+
     lxpUser.login(username, password, function(err, res) {
       if (err) {
+        alert('账户或密码错误！');
         throwError('账户或密码错误！');
         return;
       }
